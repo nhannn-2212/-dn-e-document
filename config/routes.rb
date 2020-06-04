cc87dec3b2042871730261d6c20826947f05e169
nhannn-2212 <nguyen.ngoc.nhan@sun-asterisk.com>
2020-06-04 10:33:29 +0700

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /vi|en/ do
     root "static_pages#home"
+
+    get "/login", to: "sessions#new", as: "login"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy", as: "logout"
   end
 end
