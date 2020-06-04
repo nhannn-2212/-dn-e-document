942a4fdaf52f07a20eb8d7386af858597ab815c6
nhannn-2212 <nguyen.ngoc.nhan@sun-asterisk.com>
2020-06-04 10:33:29 +0700

    View PDF

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+
   def new; end
 
   def create; end
+
+  def show
+    @documents = @user.documents.by_title.paginate(page: params[:page], per_page: Settings.per_page)
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:danger] = t "error.invalid_ID"
+    redirect_to root_url
+  end
 end
