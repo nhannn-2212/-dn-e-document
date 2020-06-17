2ce665891f4eeff546db6618d29445c49aa6cb6a
nhannn-2212 <nguyen.ngoc.nhan@sun-asterisk.com>
2020-06-17 11:25:07 +0700

    Deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(fullname: "Example User",
   role: 2,
   active: true,
   coin: 0)
-30.times do |n|
+10.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@gmail.com"
   password = "password"
@@ -23,10 +23,4 @@ user = User.first
 10.times do
   name = Faker::Book.title
   user.categories.create!(name: name)
-end
-
-users = User.take(3)
-5.times do |n|
-  content = Faker::Lorem.sentence(5)
-  users.each{|user| user.comments.create!(content: content, document_id: Document.first.id)}
 end
